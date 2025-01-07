f4d068a7ba9f7299d9a3dc237658fb7e72412f54
nalendev <nalendrapraja11@gmail.com>
2025-01-08 00:17:04 +0800

    add sandbox

---
+++ 00-sandbox/sandbox.asm
@@ -0,0 +1,15 @@
+section .data
+
+section .text
+
+global main
+main:
+    mov rbp, rsp    ; save stack pointer for debugger
+    nop
+    
+    ; Put code between two nops ....
+    
+    ; Put code between two nops ....
+    nop
+    
+section .bss
